91b4a28603fe56c8df109cb86102f8cdbaedba4f
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-05-15 20:23:57 -0300

    first commit

---
+++ src/model/venda.js
@@ -0,0 +1,9 @@
+const VendaController = require('../controller/venda');
+
+class VendaApi {
+  async criarVenda(req, res) {
+    const Titulo = req.body.titulo;
+    const Categoria = req.body.categoria;
+    const 
+  }
+}
