a18b55d394894ae9f3cc4fb8e35efa095f6af240
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-05-15 20:44:47 -0300

    update

--- Produto/src/model/venda.js
+++
@@ -1,9 +0,0 @@
-const VendaController = require('../controller/venda');
-
-class VendaApi {
-  async criarVenda(req, res) {
-    const Titulo = req.body.titulo;
-    const Categoria = req.body.categoria;
-    const 
-  }
-}
